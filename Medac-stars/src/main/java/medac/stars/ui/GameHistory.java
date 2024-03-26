7cee1917d397513cdaa841cf4089b9f19d21ce5c
Cristian Olivares (visibait) <grafoplays@gmail.com>
2024-03-26 09:46:08 +0100

    tweak(GameHistory): Remove dragging on table + add custom font on header & content

--- Medac-stars/src/main/java/medac/stars/ui/GameHistory.java
+++ Medac-stars/src/main/java/medac/stars/ui/GameHistory.java
@@ -1,7 +1,11 @@
 package medac.stars.ui;
 
+import medac.stars.utils.FontBuilder;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
+import java.awt.*;
 
 public class GameHistory extends JFrame {
     private JPanel GameHistoryPanel;
@@ -13,7 +17,7 @@ public class GameHistory extends JFrame {
         add(GameHistoryPanel);
         setTitle("MEDAC Stars - Game History");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 500);
+        setSize(900, 500);
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -34,7 +38,10 @@ public class GameHistory extends JFrame {
         };
 
         table1 = new JTable(data, columnNames);
+
+        // Deshabilitamos la edición y el drag
         table1.setEnabled(false);
+        table1.getTableHeader().setReorderingAllowed(false);
 
         // Alineamos las celdas al centro
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
@@ -43,6 +50,10 @@ public class GameHistory extends JFrame {
             table1.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
         }
 
+        Font montserratSemiBold = FontBuilder.getFont("Montserrat-SemiBold.ttf");
+        table1.setFont(montserratSemiBold.deriveFont(12f));
 
+        JTableHeader header = table1.getTableHeader();
+        header.setFont(montserratSemiBold.deriveFont(14f));
     }
 }
