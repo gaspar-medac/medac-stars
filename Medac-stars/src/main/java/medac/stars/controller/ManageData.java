ce989857e5158c154ae7968899fdd487bc36693a
Asier Señor Díaz <asd0026@alu.medac.es>
2024-03-26 09:36:19 +0100

    Interfaces Añadir y Editar
    no tiene funcionalidad implementada

--- Medac-stars/src/main/java/medac/stars/controller/ManageData.java
+++ Medac-stars/src/main/java/medac/stars/controller/ManageData.java
@@ -21,4 +21,21 @@ public class ManageData {
     public static Set<MedacStar> medacStarSet = new HashSet<MedacStar>();
     public static ArrayList<Game> gameList = new ArrayList<Game>();
     public static HashMap<User, Chat> chatHashMap = new HashMap<User, Chat>();    
+    
+    static {
+        arenaSet.add(new Arena("arena1", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena2", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena3", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena4", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena5", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena6", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena7", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena8", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena9", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena10", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena11", false, "bla", "", 10));
+        arenaSet.add(new Arena("arena12", false, "bla", "", 10));
+        
+        
+    }
 }
