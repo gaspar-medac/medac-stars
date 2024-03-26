1945305d337807a0e525a9e7a73005a56dc87c4d
Vicente Hernández Maciá <vhm0006@alu.medac.es>
2024-03-26 14:19:09 +0100

    Visualización de las arenas en tabla
    Lista en la tabla las arenas con los botones para editar respectivamente dichas arenas.

---
+++ Medac-stars/src/main/java/medac/stars/ui/arena/ButtonEditor.java
@@ -0,0 +1,74 @@
+package medac.stars.ui.arena;
+
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+
+class ButtonEditor extends DefaultCellEditor {
+
+    protected JButton button;
+    private String label;
+
+    //aqui se puede indicar indice de la tabla, id del objeto medac star...
+    private int numeroFila;
+
+    // es pulsado
+    private boolean isPushed;
+
+    public ButtonEditor(JCheckBox checkBox) {
+        super(checkBox);
+        button = new JButton();
+        button.setOpaque(true);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fireEditingStopped();
+            }
+        });
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value,
+            boolean isSelected, int row, int column) {
+        if (isSelected) {
+            button.setForeground(table.getSelectionForeground());
+            button.setBackground(table.getSelectionBackground());
+        } else {
+            button.setForeground(table.getForeground());
+            button.setBackground(table.getBackground());
+        }
+        label = (value == null) ? "" : value.toString();
+        button.setText(label);
+        numeroFila = row;
+        isPushed = true;
+        return button;
+    }
+
+    @Override
+    public Object getCellEditorValue() {
+        if (isPushed) {
+            JOptionPane.showMessageDialog(button, label + ": pulsado: " + numeroFila);
+        }
+        isPushed = false;
+        //devolvemos el label, pero podria ser el numero de fila
+        return new String(label);
+    }
+
+    @Override
+    public boolean stopCellEditing() {
+        isPushed = false;
+        return super.stopCellEditing();
+    }
+
+    @Override
+    protected void fireEditingStopped() {
+        super.fireEditingStopped();
+    }
+}
