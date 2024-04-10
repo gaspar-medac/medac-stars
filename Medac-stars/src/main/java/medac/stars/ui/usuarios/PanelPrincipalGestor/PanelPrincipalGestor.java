1862d232c6611a15fa6283adb8ec9fb044513d08
razor <cas0014@alu.medac.es>
2024-04-10 14:14:19 +0200

    Implementación de comentarios en PanelPrincipalGestores

--- Medac-stars/src/main/java/medac/stars/ui/usuarios/PanelPrincipalGestor/PanelPrincipalGestor.java
+++ Medac-stars/src/main/java/medac/stars/ui/usuarios/PanelPrincipalGestor/PanelPrincipalGestor.java
@@ -18,14 +18,20 @@ public class PanelPrincipalGestor extends JFrame{
 
     private User usuario;
 
+    /**
+     * Constructor por defecto del panel principal
+     */
     public PanelPrincipalGestor(){
-        add(panelPrincipal);
+        add(panelPrincipal);// Lanza la ventana
         setTitle("TestUI"); //Título de la ventana
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//Establece como se van a cerrar las ventanas
-        setSize(700, 500);
+        setSize(700, 500);//Establece el tamaño de la ventana
         setLocationRelativeTo(null);
         setVisible(true);
 
+        /**
+         * Hace aparecer la creación de usuarios al dar al botón de añadir
+         */
         bAnyadirUsuario.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -33,6 +39,10 @@ public class PanelPrincipalGestor extends JFrame{
                dispose();
             }
         });
+
+        /**
+         * Hace que se lance la tabla de edición de usuarios al pulsar el botón
+         */
         bEditarUsuarios.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -42,7 +52,10 @@ public class PanelPrincipalGestor extends JFrame{
         });
     }
 
-
+    /**
+     * Main del panel principal
+     * @param Args
+     */
     public static void main(String[] Args){
         new PanelPrincipalGestor();
     }
