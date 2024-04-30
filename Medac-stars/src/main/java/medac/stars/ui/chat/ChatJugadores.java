9a980f3df4390cd107e4a22b0878b73bafaf452f
Jesus <jmm0108@alu.medac.es>
2024-04-30 14:25:22 +0200

    Cambio del nombre JPanel
    Se ha cambiado el nombre del JPanel para que sea mas descriptivo

--- Medac-stars/src/main/java/medac/stars/ui/chat/ChatJugadores.java
+++ Medac-stars/src/main/java/medac/stars/ui/chat/ChatJugadores.java
@@ -49,22 +49,22 @@ public class ChatJugadores extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jPanel1 = new javax.swing.JPanel();
+        jChatJugador = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jChatJugadorLayout = new javax.swing.GroupLayout(jChatJugador);
+        jChatJugador.setLayout(jChatJugadorLayout);
+        jChatJugadorLayout.setHorizontalGroup(
+            jChatJugadorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 615, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jChatJugadorLayout.setVerticalGroup(
+            jChatJugadorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 349, Short.MAX_VALUE)
         );
 
-        jScrollPane1.setViewportView(jPanel1);
+        jScrollPane1.setViewportView(jChatJugador);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -123,7 +123,7 @@ public class ChatJugadores extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jChatJugador;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 
