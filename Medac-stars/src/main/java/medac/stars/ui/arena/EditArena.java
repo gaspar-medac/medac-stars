d8f7faa89ead5d1166faf0af6f7fa4b999e8cbf6
Vicente Hernández Maciá <vhm0006@alu.medac.es>
2024-05-21 19:45:51 +0200

    EditArena.java Renombrado de spinner y labels + optimizacion imports
    renombrado de todos los label a nombres acordes a su contenido y del spinner a contObstaculosEditar

--- Medac-stars/src/main/java/medac/stars/ui/arena/EditArena.java
+++ Medac-stars/src/main/java/medac/stars/ui/arena/EditArena.java
@@ -37,7 +37,7 @@ public class EditArena extends javax.swing.JFrame {
         tfEditarNombre.setText(arena.getName());
         cbCuentaAtrasEditar.setSelected(arena.isCountdown());
         tfEditarDescripcion.setText(arena.getDescription());
-        jSpinner1.setValue(arena.getBarriers());
+        contObstaculosEditar.setValue(arena.getBarriers());
         jlimagenArena.setIcon(arena.getImage());
         this.index = index;
         this.tablaArenas = tablaArenas;
@@ -54,15 +54,15 @@ public class EditArena extends javax.swing.JFrame {
 
         bCancelarEditar = new javax.swing.JButton();
         bCambiarImagen = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        labelNombre = new javax.swing.JLabel();
         tfEditarNombre = new javax.swing.JTextField();
         cbCuentaAtrasEditar = new javax.swing.JCheckBox();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        labelNumeroObstaculos = new javax.swing.JLabel();
+        labelDescripcion = new javax.swing.JLabel();
         tfEditarDescripcion = new javax.swing.JTextField();
         bGuardarCambios = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jSpinner1 = new javax.swing.JSpinner();
+        jTitulo = new javax.swing.JLabel();
+        contObstaculosEditar = new javax.swing.JSpinner();
         jlimagenArena = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -83,8 +83,8 @@ public class EditArena extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
-        jLabel1.setText("Nombre");
+        labelNombre.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        labelNombre.setText("Nombre");
 
         tfEditarNombre.setEditable(false);
         tfEditarNombre.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
@@ -102,11 +102,11 @@ public class EditArena extends javax.swing.JFrame {
             }
         });
 
-        jLabel2.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
-        jLabel2.setText("Numero de Obstáculos");
+        labelNumeroObstaculos.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        labelNumeroObstaculos.setText("Numero de Obstáculos");
 
-        jLabel3.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
-        jLabel3.setText("Descripción");
+        labelDescripcion.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        labelDescripcion.setText("Descripción");
 
         tfEditarDescripcion.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
 
@@ -118,8 +118,8 @@ public class EditArena extends javax.swing.JFrame {
             }
         });
 
-        jLabel4.setFont(new java.awt.Font("Comic Sans MS", 0, 36)); // NOI18N
-        jLabel4.setText("Editar Arena");
+        jTitulo.setFont(new java.awt.Font("Comic Sans MS", 0, 36)); // NOI18N
+        jTitulo.setText("Editar Arena");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -141,44 +141,44 @@ public class EditArena extends javax.swing.JFrame {
                         .addGap(74, 74, 74)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jLabel1)
-                                .addComponent(jLabel2)
-                                .addComponent(jLabel3)
+                                .addComponent(labelNombre)
+                                .addComponent(labelNumeroObstaculos)
+                                .addComponent(labelDescripcion)
                                 .addComponent(tfEditarDescripcion)
                                 .addComponent(tfEditarNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(cbCuentaAtrasEditar)
-                            .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(contObstaculosEditar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(133, 133, 133))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel4)
+                        .addComponent(jTitulo)
                         .addGap(239, 239, 239))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(11, 11, 11)
-                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jTitulo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel1)
+                                .addComponent(labelNombre)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(tfEditarNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addComponent(cbCuentaAtrasEditar)
                                 .addGap(18, 18, 18)
-                                .addComponent(jLabel2)
+                                .addComponent(labelNumeroObstaculos)
                                 .addGap(9, 9, 9)
-                                .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(contObstaculosEditar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(3, 3, 3)
                                 .addComponent(jlimagenArena, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(18, 18, 18)
                         .addComponent(bCambiarImagen, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
+                        .addComponent(labelDescripcion)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(tfEditarDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(40, 40, 40)
@@ -201,7 +201,7 @@ public class EditArena extends javax.swing.JFrame {
      * @param evt 
      */
     private void bGuardarCambiosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bGuardarCambiosActionPerformed
-        Arena arena = new Arena(tfEditarNombre.getText(), cbCuentaAtrasEditar.isSelected(), tfEditarDescripcion.getText(), jlimagenArena.getIcon(), (int) jSpinner1.getValue());
+        Arena arena = new Arena(tfEditarNombre.getText(), cbCuentaAtrasEditar.isSelected(), tfEditarDescripcion.getText(), jlimagenArena.getIcon(), (int) contObstaculosEditar.getValue());
         for (int i = 0; i < arenaSet.size(); i++) {
             if (i == index) {
                 arenaSet.set(index, arena);
@@ -263,12 +263,12 @@ public class EditArena extends javax.swing.JFrame {
     private javax.swing.JButton bCancelarEditar;
     private javax.swing.JButton bGuardarCambios;
     private javax.swing.JCheckBox cbCuentaAtrasEditar;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JSpinner jSpinner1;
+    private javax.swing.JSpinner contObstaculosEditar;
+    private javax.swing.JLabel jTitulo;
     private javax.swing.JLabel jlimagenArena;
+    private javax.swing.JLabel labelDescripcion;
+    private javax.swing.JLabel labelNombre;
+    private javax.swing.JLabel labelNumeroObstaculos;
     private javax.swing.JTextField tfEditarDescripcion;
     private javax.swing.JTextField tfEditarNombre;
     // End of variables declaration//GEN-END:variables
