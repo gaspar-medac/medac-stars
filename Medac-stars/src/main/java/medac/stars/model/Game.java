bc17e381d9b67cfbadd97dfd40790fd5537df2f0
LoadingLio <esv0009@alu.medac.es>
2024-05-28 13:31:14 +0200

    Comments
    Comentarios añadidos en Game

--- Medac-stars/src/main/java/medac/stars/model/Game.java
+++ Medac-stars/src/main/java/medac/stars/model/Game.java
@@ -5,54 +5,61 @@
 package medac.stars.model;
 
 /**
- *
- * @author gaspar
+ * Clase que representa un juego entre dos equipos.
+ * Permite almacenar y obtener los equipos y el resultado del juego.
  */
 public class Game {
-    private Team team1;
-    private Team team2;
+    private Team team1; // Primer equipo
+    private Team team2; // Segundo equipo
     
     /**
-     * 0: team1 win
-     * 1: team2 win
-     * 2: draw //optional
+     * Resultado del juego:
+     * 0: gana el equipo1
+     * 1: gana el equipo2
+     * 2: empate (opcional)
      */
     private int result;
 
+    // Constructor que inicializa los equipos sin el resultado
     public Game(Team team1, Team team2) {
         this.team1 = team1;
         this.team2 = team2;
     }
 
+    // Constructor que inicializa los equipos y el resultado
     public Game(Team team1, Team team2, int result) {
         this.team1 = team1;
         this.team2 = team2;
         this.result = result;
     }
 
+    // Obtener el primer equipo
     public Team getTeam1() {
         return team1;
     }
 
+    // Establecer el primer equipo
     public void setTeam1(Team team1) {
         this.team1 = team1;
     }
 
+    // Obtener el segundo equipo
     public Team getTeam2() {
         return team2;
     }
 
+    // Establecer el segundo equipo
     public void setTeam2(Team team2) {
         this.team2 = team2;
     }
 
+    // Obtener el resultado del juego
     public int getResult() {
         return result;
     }
 
+    // Establecer el resultado del juego
     public void setResult(int result) {
         this.result = result;
     }
-    
-    
 }
