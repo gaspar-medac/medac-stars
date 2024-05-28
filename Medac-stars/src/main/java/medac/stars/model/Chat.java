6786c2dd00ab3b870764707fd86e96e310f9e4a7
LoadingLio <esv0009@alu.medac.es>
2024-05-28 13:49:08 +0200

    Comentarios y cambio
    He comentado Chat y he cambiado unas list por arraylist

--- Medac-stars/src/main/java/medac/stars/model/Chat.java
+++ Medac-stars/src/main/java/medac/stars/model/Chat.java
@@ -1,30 +1,48 @@
 package medac.stars.model;
 
-import java.awt.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- *
- * @author gaspar
+ * Clase que representa un chat, compuesto por una lista de mensajes.
+ * 
+ * @autor gaspar
  */
 public class Chat {
 
-    private ArrayList<Message> messages;
+    private List<Message> messages; // Lista de mensajes en el chat
 
-    
+    /**
+     * Constructor que inicializa una lista vacía de mensajes.
+     */
     public Chat() {
         this.messages = new ArrayList<>();
     }
-    public Chat(ArrayList  messages) {
+
+    /**
+     * Constructor que inicializa el chat con una lista dada de mensajes.
+     * 
+     * @param messages Lista de mensajes inicial
+     */
+    public Chat(List<Message> messages) {
         this.messages = messages;
     }
 
-    public ArrayList getMessages() {
+    /**
+     * Obtener la lista de mensajes.
+     * 
+     * @return Lista de mensajes
+     */
+    public List<Message> getMessages() {
         return messages;
     }
 
-    public void setMessages(ArrayList messages) {
+    /**
+     * Establecer la lista de mensajes.
+     * 
+     * @param messages Lista de mensajes
+     */
+    public void setMessages(List<Message> messages) {
         this.messages = messages;
     }
-
 }
