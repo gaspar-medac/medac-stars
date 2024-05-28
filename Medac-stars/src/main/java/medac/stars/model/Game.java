9a467dbdf70b46c1ff9c796defd9012729a4ac96
LoadingLio <esv0009@alu.medac.es>
2024-05-28 13:35:39 +0200

    Refactorizado el código Game
    - Añadidos comentarios de documentación (Javadoc) a los métodos y constructores.
    - Eliminados los comentarios automáticos generados por el IDE.
    - Simplificada la estructura del código para mantenerla limpia y concisa.
    - No se realizaron cambios en la lógica del programa.

--- Medac-stars/src/main/java/medac/stars/model/Game.java
+++ Medac-stars/src/main/java/medac/stars/model/Game.java
@@ -1,17 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package medac.stars.model;
 
 /**
  * Clase que representa un juego entre dos equipos.
- * Permite almacenar y obtener los equipos y el resultado del juego.
  */
 public class Game {
     private Team team1; // Primer equipo
     private Team team2; // Segundo equipo
-    
+
     /**
      * Resultado del juego:
      * 0: gana el equipo1
@@ -20,45 +15,80 @@ public class Game {
      */
     private int result;
 
-    // Constructor que inicializa los equipos sin el resultado
+    /**
+     * Constructor que inicializa los equipos sin el resultado.
+     *
+     * @param team1 Primer equipo
+     * @param team2 Segundo equipo
+     */
     public Game(Team team1, Team team2) {
         this.team1 = team1;
         this.team2 = team2;
     }
 
-    // Constructor que inicializa los equipos y el resultado
+    /**
+     * Constructor que inicializa los equipos y el resultado.
+     *
+     * @param team1  Primer equipo
+     * @param team2  Segundo equipo
+     * @param result Resultado del juego
+     */
     public Game(Team team1, Team team2, int result) {
         this.team1 = team1;
         this.team2 = team2;
         this.result = result;
     }
 
-    // Obtener el primer equipo
+    /**
+     * Obtener el primer equipo.
+     *
+     * @return Primer equipo
+     */
     public Team getTeam1() {
         return team1;
     }
 
-    // Establecer el primer equipo
+    /**
+     * Establecer el primer equipo.
+     *
+     * @param team1 Primer equipo
+     */
     public void setTeam1(Team team1) {
         this.team1 = team1;
     }
 
-    // Obtener el segundo equipo
+    /**
+     * Obtener el segundo equipo.
+     *
+     * @return Segundo equipo
+     */
     public Team getTeam2() {
         return team2;
     }
 
-    // Establecer el segundo equipo
+    /**
+     * Establecer el segundo equipo.
+     *
+     * @param team2 Segundo equipo
+     */
     public void setTeam2(Team team2) {
         this.team2 = team2;
     }
 
-    // Obtener el resultado del juego
+    /**
+     * Obtener el resultado del juego.
+     *
+     * @return Resultado del juego
+     */
     public int getResult() {
         return result;
     }
 
-    // Establecer el resultado del juego
+    /**
+     * Establecer el resultado del juego.
+     *
+     * @param result Resultado del juego
+     */
     public void setResult(int result) {
         this.result = result;
     }
