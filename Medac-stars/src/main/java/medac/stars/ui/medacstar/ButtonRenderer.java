67db1b53097fda82dbce48bee47fe7cdef512136
Samuel mejias <sms0024@alu.medac.es>
2024-05-29 09:05:33 +0200

    Override en ButtonRenderer
    he añadido la anotacion Override en ButtonRenderer para mejorar la legibilidad.

--- Medac-stars/src/main/java/medac/stars/ui/medacstar/ButtonRenderer.java
+++ Medac-stars/src/main/java/medac/stars/ui/medacstar/ButtonRenderer.java
@@ -13,6 +13,7 @@ class ButtonRenderer extends JButton implements TableCellRenderer {
       setOpaque(true);
     }
   
+    @Override
     public Component getTableCellRendererComponent(JTable table, Object value,
         boolean isSelected, boolean hasFocus, int row, int column) {
       if (isSelected) {
